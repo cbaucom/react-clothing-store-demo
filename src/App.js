6f385254eb7f27d5902cc9e8afc6e72b53cbc126
Chris Baucom <crbaucom@gmail.com>
2019-07-08 20:51:00 -0400

    Add initial homepage layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./homepage.styles.scss";
+import HomePage from "./homepage.component";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;

5177a1cea2e08f39d2ed2eb3410723308b792887
Chris Baucom <crbaucom@gmail.com>
2019-07-28 16:12:47 -0400

    Add ability to increase/decrease quanity and remove item from cart dropdown

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,20 +1,42 @@
 import React from "react";
+import { connect } from 'react-redux'
+
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions'
+
 import {
   CartItemContainer,
   CartItemImage,
   ItemDetailsContainer,
+  QuantityContainer,
+  TextContainer,
+  RemoveButtonContainer,
 } from "./cart-item.styles";
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+const CartItem = ({ item, clearItem, addItem, removeItem }) => {
+	const { name, imageUrl, price, quantity } = item;
+
+  return  (
   <CartItemContainer>
     <CartItemImage src={imageUrl} alt={name} />
     <ItemDetailsContainer>
-      <span>{name}</span>
-      <span>
-        {quantity} x ${price}
-      </span>
+      <TextContainer>{name}</TextContainer>
+      <QuantityContainer>
+      <div onClick={() => removeItem(item)}>&#10094;</div>
+			<span>{quantity}</span>
+			<div onClick={() => addItem(item)}>&#10095;</div>
+      <span> x ${price}</span>
+      </QuantityContainer>
     </ItemDetailsContainer>
+    <RemoveButtonContainer onClick={() => clearItem(item)}>
+      &#10005;
+    </RemoveButtonContainer>
   </CartItemContainer>
-);
+)};
 
-export default CartItem;
+const mapDispatchToProps = dispatch => ({
+	clearItem: item => dispatch(clearItemFromCart(item)),
+	addItem: item => dispatch(addItem(item)),
+	removeItem: item => dispatch(removeItem(item)),
+})
+
+export default connect(null, mapDispatchToProps)(CartItem);
--- src/components/cart-item/cart-item.styles.jsx
+++ src/components/cart-item/cart-item.styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CartItemContainer = styled.div`
   width: 100%;
@@ -17,5 +17,28 @@ export const ItemDetailsContainer = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  padding: 10px 20px;
+  /* padding: 10px 0 10px 10px; */
+`;
+
+export const TextContainer = styled.span`
+  padding: 0 10px 10px 10px;
+`;
+
+export const QuantityContainer = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    margin: 0 5px;
+  }
+  div {
+    cursor: pointer;
+    padding: 10px;
+  }
+`;
+
+export const RemoveButtonContainer = styled.div`
+  cursor: pointer;
+  align-items: center;
+  display: flex;
+  font-size: 14px;
 `;

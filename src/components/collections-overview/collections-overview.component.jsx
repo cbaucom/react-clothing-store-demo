a9bee0ea8b45c93a5f1f4a840d0060ca5375691a
Chris Baucom <crbaucom@gmail.com>
2019-07-27 21:54:48 -0400

    Add selector for collection preview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selectors';
 
 import { CollectionsOverviewContainer } from './collections-overview.styles';
 
@@ -17,7 +17,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);

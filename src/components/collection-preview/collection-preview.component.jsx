a7a0ed9debe558b201029a88f37ed787d78e234a
Chris Baucom <crbaucom@gmail.com>
2019-07-30 15:50:06 -0400

    Add route link to title

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import CollectionItem from "../collection-item/collection-item.component";
-import { CollectionPreviewContainer, TitleContainer, PreviewContainer } from "./collection-preview.styles";
 
-const CollectionPreview = ({ title, items }) => (
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  PreviewContainer,
+} from "./collection-preview.styles";
+
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
   <CollectionPreviewContainer>
-    <TitleContainer>{title.toUpperCase()}</TitleContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
+      {title.toUpperCase()}
+    </TitleContainer>
     <PreviewContainer>
-			{items
-				.filter((item, idx) => idx < 4)
-				.map((item) => (
-        <CollectionItem key={item.id} item={item} />
-      ))}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </PreviewContainer>
   </CollectionPreviewContainer>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
